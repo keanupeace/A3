24d5f90d3e4a75106447105733c43df785dac41c
keanupeace <kpsenicn@confederationcollege.ca>
2024-11-20 16:04:57 -0500

    added itme fun stuff

---
+++ src/routes/Todo.svelte
@@ -0,0 +1,30 @@
+<script>
+let todoItem = $state('');
+let todoList = $state([]);
+
+function addItem() {
+     event.preventDefault()
+     if (todoItem == '') {
+          return;
+     }
+     todoList.push(todoItem);
+     todoItem = '';
+}
+$inspect(todoList);
+
+</script>
+<form onsubmit={addItem}> 
+<input type="text" bind:value={todoItem}>
+<button type="submit"> Add</button>
+</form>
+
+<ul>
+     {#each todoList as Item}
+          <li>{Item}</li>
+     {/each}
+</ul>
+
+<style>
+
+
+</style>
